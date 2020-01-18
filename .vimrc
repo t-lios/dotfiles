5def878e10165ba0db87871f41a1f54402114ccb
lios <yoshi29t83@gmail.com>
2020-01-18 14:29:15 +0900

    update

---
+++ .vimrc
@@ -0,0 +1,15 @@
+" 行番号を表示
+:set number
+:set title
+
+" ファイルを上書きする前にバックアップ作成を無効化
+:set nowritebackup
+:set nobackup
+
+" 挿入モードでバックスペースで削除
+set backspace=indent,eol,start
+
+" シンタックスハイライトを有効にする
+syntax on
+colorscheme molokai
+set t_Co=256
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc)
+DOT_FILES=(.bashrc .vimrc .vim)
 
-for file in ${DOT_FILES}
+for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s -f $HOME/dotfiles/$file $HOME/$file
 done
